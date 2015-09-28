886133dc1238a30de55e269f98514a6c59bc5a9f
thomasmccarthy <thomasmccarthy96@gmail.com>
2015-09-28 09:55:18 -0400

    Added Toast
    Added toast to button on click

--- GitDemo/app/src/main/java/com/cs616/gitdemo/MainActivity.java
+++ GitDemo/app/src/main/java/com/cs616/gitdemo/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pressMe = (Button) findViewById(R.id.btn_press_me);
+
+        pressMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Toast", Toast.LENGTH_SHORT); //.show();
+            }
+        });
     }
 
     @Override
